8f8dda28e93d8152c42935208691273484f368d0
Nick Terwoord <nick@triggit.com>
2015-04-19 18:07:32 -0400

    Cleanup API
    - Added rough documentation
    - Added helper method, `reply`, to reply to conversation
    - Added JSON envelope
    - Improved Error Handling
    - Implemented Patch method
    - Have Create-like methods (e.g. PATCH and POST) return DB object through the magic of `Returning`

---
+++ db/errors.go
@@ -0,0 +1,16 @@
+package db
+
+type DBError string
+
+func (e DBError) Error() string {
+	return string(e)
+}
+
+const (
+	ErrConnection DBError = "DB Connection"
+	ErrRowScan    DBError = "Row Scan"
+	ErrRowUnknown DBError = "Row Unknown"
+	ErrRowDelete  DBError = "Row Delete"
+	ErrRowCreate  DBError = "Row Create"
+	ErrRowUpdate  DBError = "Row Update"
+)
